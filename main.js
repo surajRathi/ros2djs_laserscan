656ab9239a1a1b38540cb80fb7274a31b2a2c4e5
Suraj Rathi <suraj.rathi00@gmail.com>
2022-06-24 16:44:51 +0530

    clean up click_catcher

--- main.js
+++ main.js
@@ -7,9 +7,6 @@ ros.on('connection', () => console.log('Connected to websocket server.'));
 ros.on('error', error => console.log('Error connecting to websocket server: ', error));
 ros.on('close', () => console.log('Connection to websocket server closed.'));
 
-const send_goal_to_actionlib = false;
-const send_goal_to_executor = true;
-
 class App {
     ground_frame = "map"
     width = 720
@@ -45,55 +42,57 @@ class App {
             divID: this.div_el_id, width: this.width, height: this.height, antialias: true
         });
 
-        this.action_client = new ROSLIB.ActionClient({
-            ros: this.ros, serverName: '/move_base', actionName: 'move_base/goal'
-        });
+
+        this.executor_pub = new ROSLIB.Topic({
+            ros: this.ros, name: "/cmd", messageType: "std_msgs/String",
+        })
+        this.executor_pub.advertise()
+
+        document.getElementById(this.sleep_button_el_id).onclick = (ev) => {
+            const sleep_obj = {
+                "cmd": "sleep", "time": parseFloat(document.getElementById(this.time_textbox_el_id).value)
+            }
+
+            this.executor_pub.publish(new ROSLIB.Message({
+                data: JSON.stringify(sleep_obj)
+            }))
+        }
 
         // You have to tao thrice quickly to activate the trigger the catcher idk why
         class ClickCatcher extends THREE.EventDispatcher {
-            constructor(mouseHandler, scene, ros, ac_client) {
+            constructor(scene, mouseHandler, pub_topic) {
                 super();
-                this.action_client = ac_client
-                this.ros = ros
+                this.pub_topic = pub_topic
                 this.mouse_handler = mouseHandler
                 this.scene = scene
                 this.marker = null
 
                 this.start = null
 
-                console.log("Made click Catcher")
-                const dummy = this.dummy.bind(this)
-                const start_dummy = this.start_dummy.bind(this)
-                this.addEventListener('mousedown', start_dummy);
-                this.addEventListener('mouseup', dummy);
-                this.addEventListener('touchstart', start_dummy);
-                this.addEventListener('touchend', dummy);
+                const on_mouse_down_dummy = this.on_mouse_down.bind(this)
+                const on_mouse_up_dummy = this.on_mouse_up.bind(this)
+                this.addEventListener('mousedown', on_mouse_down_dummy);
+                this.addEventListener('touchstart', on_mouse_down_dummy);
+
+                this.addEventListener('mouseup', on_mouse_up_dummy);
+                this.addEventListener('touchend', on_mouse_up_dummy);
 
                 // TODO: Show the arrow when they are dragging
                 // this.addEventListener('mousemove', dummy);
                 // this.addEventListener('touchmove', dummy);
 
-                if (send_goal_to_executor) {
-                    this.pub_topic = new ROSLIB.Topic({
-                        ros: this.ros, name: "/cmd", messageType: "std_msgs/String",
-
-                    })
-                    this.pub_topic.advertise()
-                }
+                this.mouse_handler.camera_controls = this.mouse_handler.fallbackTarget
+                this.mouse_handler.click_catcher = this
             }
 
-            start_dummy(ev) {
+            on_mouse_down(ev) {
                 if (ev.type === 'mousedown' || ev.type === 'touchstart') {
                     this.start = ROS3D.intersectPlane(ev.mouseRay, new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 0, 1))
-                    console.log(this.start)
                 }
             }
 
-            dummy(ev) {
+            on_mouse_up(ev) {
                 if (ev.type === 'mouseup' || ev.type === 'touchend') {
-                    console.log("dummy callsed!!")
-                    // console.log(ev);
-
                     const position = ROS3D.intersectPlane(ev.mouseRay, new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 0, 1))
                     const theta = Math.atan2(position.y - this.start.y, position.x - this.start.x)
 
@@ -112,82 +111,26 @@ class App {
                     // Disable it
                     this.mouse_handler.fallbackTarget = this.mouse_handler.camera_controls
 
-                    if (send_goal_to_actionlib) {
-                        const currentTime = new Date();
-                        const secs = Math.floor(currentTime.getTime() / 1000);
-                        const nsecs = Math.round(1000000000 * (currentTime.getTime() / 1000 - secs));
-                        const goal = new ROSLIB.Goal({
-                            actionClient: this.action_client, goalMessage: {
-                                target_pose: {
-                                    header: {
-                                        frame_id: 'map', stamp: {
-                                            secs: secs, nsecs: nsecs
-                                        }
-                                    },
-
-                                    pose: {
-                                        position: {
-                                            x: this.start.x, y: this.start.y, z: this.start.z
-                                        }, orientation: {
-                                            x: 0, y: 0, z: Math.sin(theta / 2), w: Math.cos(theta / 2)
-                                        }
-                                    }
-
-                                }
-                            }
-                        });
-
-                        goal.on('feedback', function (feedback) {
-                            console.log('Feedback: ' + feedback.sequence);
-                        });
-
-                        goal.on('result', function (result) {
-                            console.log('Final Result: ' + result.sequence);
-                        });
-
-                        goal.send();
-                    }
-
-                    // TODO: Dont do string stuff, use native js objects and the JSON library
-                    if (send_goal_to_executor) {
-                        const send_goal_obj = {
-                            "cmd": "send_goal", "header": {"frame_id": "/map"}, "pose": {
-                                "position": {"x": this.start.x, "y": this.start.y, "z": this.start.z},
-                                "orientation": {"x": 0.0, "y": 0.0, "z": Math.sin(theta / 2)}
-                            }
+                    const send_goal_obj = {
+                        "cmd": "send_goal", "header": {"frame_id": "/map"}, "pose": {
+                            "position": {"x": this.start.x, "y": this.start.y, "z": this.start.z},
+                            "orientation": {"x": 0.0, "y": 0.0, "z": Math.sin(theta / 2)}
                         }
-
-                        this.pub_topic.publish(new ROSLIB.Message({
-                            data: JSON.stringify(send_goal_obj)
-                        }))
                     }
+
+                    this.pub_topic.publish(new ROSLIB.Message({
+                        data: JSON.stringify(send_goal_obj)
+                    }))
                 }
 
             }
         }
 
-        this.cc = new ClickCatcher(this.viewer.highlighter.mouseHandler, this.viewer.scene, this.ros, this.action_client)
-        this.viewer.highlighter.mouseHandler.camera_controls = this.viewer.highlighter.mouseHandler.fallbackTarget
-        this.viewer.highlighter.mouseHandler.click_catcher = this.cc
+        this.cc = new ClickCatcher(this.viewer.scene, this.viewer.highlighter.mouseHandler, this.executor_pub)
 
-        document.getElementById(this.goal_button_el_id).onclick = (ev) => {
+
+        document.getElementById(this.goal_button_el_id).onclick = () => {
             this.viewer.highlighter.mouseHandler.fallbackTarget = this.viewer.highlighter.mouseHandler.click_catcher
-        }
-
-
-        this.executor_pub = new ROSLIB.Topic({
-            ros: this.ros, name: "/cmd", messageType: "std_msgs/String",
-
-        })
-        this.executor_pub.advertise()
-        document.getElementById(this.sleep_button_el_id).onclick = (ev) => {
-            const sleep_obj = {
-                "cmd": "sleep", "time": parseFloat(document.getElementById(this.time_textbox_el_id).value)
-            }
-
-            this.executor_pub.publish(new ROSLIB.Message({
-                data: JSON.stringify(sleep_obj)
-            }))
         }
 
         // Set up the map client.
